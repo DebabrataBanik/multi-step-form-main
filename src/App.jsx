827c37606d264f8a3d453d7575e84c5560708194
DebabrataBanik <debabratabanik02@gmail.com>
2026-03-19 00:11:44 +0530

    style(ui): base layout UI for smaller screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import Form from "./components/Form"
+import Steps from "./components/Steps"
+import Footer from "./components/Footer"
+
 export default function App(){
   return (
-    <div>
-      App
+    <div className="wrapper">
+      <div className="container">
+        <Steps />
+        <Form />
+        <Footer>
+          <button className="btn">Next Step</button>
+        </Footer>
+      </div>
     </div>
   )
 }
---
+++ src/components/Steps.jsx
@@ -0,0 +1,12 @@
+export default function Steps(){
+  return (
+    <div className="steps-container">
+      <ul>
+        <li className="step">1</li>
+        <li className="step">2</li>
+        <li className="step">3</li>
+        <li className="step">4</li>
+      </ul>
+    </div>
+  )
+}
